8541d52426c687b83082d6574c358ac7fff0325d
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-01-27 15:40:20 +0100

    try using bdist_wheel and pip to build package

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 if [[ ${HOST} =~ .*darwin.* ]]; then
-	$PYTHON setup.py build --no-openmp install
+	$PYTHON setup.py build --no-openmp bdist_wheel
+	$PYTHON -m pip install --pre --find-links dist/ --no-cache-dir --no-index --no-deps --ignore-installed -vv pyfai
+
 else
 	$PYTHON -m pip install . --no-deps --ignore-installed -vv
 fi
